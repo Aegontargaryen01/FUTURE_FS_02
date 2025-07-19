91a9bf90006ab46e841cbab69a944b25a3317731
Aegontargaryen01 <shub241104@gmail.com>
2025-07-19 19:37:02 +0530

    initial commit

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+        <br />
+        <p>Now available on andoid and IOS platforms <br /> Tomato</p>
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default AppDownload
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here </h2>
+            <p>Choose from a diverse menu featuring a number of dishes and cuisines from around the world, prepared with hygenic measures, safe materials and none the less, a lot of love </p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
